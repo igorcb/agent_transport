4a21317c3c45ac1c16a2b8f52ceb1c31d9ddfe9c
Igor C. Batista <igor.batista@gmail.com>
2018-05-29 12:04:29 -0300

    refectoring validates

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,5 @@
 class Client < ApplicationRecord
-	validates :first_name, presence: true
-	validates :last_name, presence: true
+	validates :first_name, :last_name, presence: true
 
 	def full_name
 		first_name + ' ' + last_name
